fc55414e92923c349b12df732e34a0caa74f6834
RobertoAsgon <robertoasgon@gmail.com>
2020-09-04 13:50:44 -0300

    att fav icon

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -5,8 +5,8 @@ import ImgResponsive from '../../assets/images/projects.png';
 function Projects() {
   return (
     <section className="projects-section slide-in-fwd-bottom">
-      <h1>Projetos Full-Stack JavaScript</h1>
-      <img src={ImgResponsive} alt="img responsive"></img>
+      <h1>Projetos Web ReactJS | NodeJS</h1>
+      <img src={ImgResponsive} alt="img responsive" freepik="https://stories.freepik.com/?_ga=2.128526796.1541234574.1599194576-1495242072.1598773429" ></img>
       <div>
         <a className="btn btn3">Front-end</a>
         <a className="btn btn3">Full-stack</a>
