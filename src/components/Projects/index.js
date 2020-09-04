c2f153f642f049296a73694dd3bfc1bd682c59f5
RobertoAsgon <robertoasgon@gmail.com>
2020-09-04 14:05:35 -0300

    att tela projetos

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -5,7 +5,8 @@ import ImgResponsive from '../../assets/images/projects.png';
 function Projects() {
   return (
     <section className="projects-section slide-in-fwd-bottom">
-      <h1>Projetos Web ReactJS | NodeJS</h1>
+      <h1>Projetos ReactJS & NodeJS</h1>
+      <span>Clean Arquiteture, Solid, DDD, TDD</span>
       <img src={ImgResponsive} alt="img responsive" freepik="https://stories.freepik.com/?_ga=2.128526796.1541234574.1599194576-1495242072.1598773429" ></img>
       <div>
         <a className="btn btn3">Front-end</a>
