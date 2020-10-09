71efdb90d4fc6411c13547d0b23d1af9a37434b8
RobertoAsgon <robertoasgon@gmail.com>
2020-10-09 15:10:35 -0300

    removendo arquivos desenecessários

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import Main from './screens/Main';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from '../components/Header/index';
+import AboutMe from '../components/AboutMe/index';
+import Trybe from '../components/Trybe/index';
+import Projects from '../components/Projects/index';
+import './App.css';
 
 function App() {
-  return <Main />
+  return ( 
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={AboutMe} />
+        <Route path="/trybe" component={Trybe} />
+        <Route path="/projects" component={Projects} />
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
